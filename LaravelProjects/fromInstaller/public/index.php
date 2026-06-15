e6c2ba30bd6a5ea8566c4ed8cdaa9ddff02e9ec2
Nithish Kumar Lakshmana Perumal <nithishkumarl168@gmail.com>
2026-06-15 12:43:11 +0530

    fix: redirect Laravel bootstrap cache to /tmp and revert debug wrappers

--- LaravelProjects/fromInstaller/public/index.php
+++ LaravelProjects/fromInstaller/public/index.php
@@ -1,14 +1,8 @@
 <?php
 
-use Illuminate\Foundation\Application;
 use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
-
-ini_set('display_errors', '1');
-ini_set('display_startup_errors', '1');
-error_reporting(E_ALL);
-echo "DEPLOYS_ARE_WORKING_OK"; exit;
 
 // Determine if the application is in maintenance mode...
 if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
@@ -19,17 +13,7 @@ if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php'))
 require __DIR__.'/../vendor/autoload.php';
 
 // Bootstrap Laravel and handle the request...
-/** @var Application $app */
-$app = require_once __DIR__.'/../bootstrap/app.php';
-
-try {
-    $response = $app->handleRequest(Request::capture());
-    $response->send();
-    $app->terminate();
-} catch (\Throwable $e) {
-    echo "ORIGINAL FATAL ERROR: " . $e->getMessage() . "\n";
-    echo $e->getTraceAsString();
-    exit(1);
-}
+(require_once __DIR__.'/../bootstrap/app.php')
+    ->handleRequest(Request::capture());
 
 
